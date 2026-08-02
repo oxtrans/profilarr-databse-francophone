2dfbdee102682e5354c62af712bd509aea22b4a5
Samir S <samir@example.com>
2026-08-02 17:34:01 +0200

    fix: resolve Radarr validation errors

--- ops/01.database.sql
+++ ops/01.database.sql
@@ -1,18 +1,6 @@
 -- Regular Expressions
-INSERT INTO regular_expressions (name, pattern) VALUES
-('Regex_VF', '(?i)\b(multi|multifrench|multi-fr|french|truefrench|vff?|vfi|vfq|fr)\b'),
-('Regex_VOSTFR', '(?i)\b(vostfr?|vost|subfrench|french\.subs?|fr\.subs?|sub\.fr|subbed\.fr)\b'),
-('Regex_FR_tier_1', '(?i)[-.](AMEN|ARK01|B@tman|BDHD|BlackAngel|BONBON|BOUBA|BOUC|Choco|Darki|Delivroozzi|FCK|FLOP|FORWARD|FoX|FRATERNiTY|FrIeNdS|FtLi|FTMVHD|Fuceo|FW|GKS|Goldenyann|HDForever|HeavyWeight|KAAZA|KTM|M@x|MARBLECAKE|MAX|MOONLY|MTDK|MUSTANG|NoLo|Obi|ONLY|OZEF|PATOPESTO|PEPiTE|PiouPiou|Psaro|Punisher694|QUEBEC63|RG|ROMKENT|Sicario|SR\-71|SUPPLY|T3KASHi|TANOSHii|TenmaLand|Tezcat74|TFA|THESYNDICATE|TiNA|Tsundere\-Raws|TyHD|TyrellCorp|YODA|Zapax)$'),
-('Regex_FR_tier_2', '(?i)[-.](AJP|ALLDAYiN|Anime\-Heart|Aoi\-Project|ATE|AW|CHiLL|COCAIN|COLL3CTiF|D4RK|DREAM|DUSTiN|Elecman|ENIGMA|FiND|Floppy|FUJiSAN|GHT|GORE|GundamGuy|HYPERION|IssouCorp|J4CK|Kaerizaki\-Fansub|KAF|KHFR|LiDHL|LKT|Maxadonf|mHDgz|MULTiPLY|MYSTERiON|N3ZUKO|Nagutos|Natsumi\-no\-Sekai|NEO|NEOSTARK|NoNE|OECUF|Onii\-ChanSub|ONLYMOViE|Owlolf|PATOMiEL|pERsO|Pikari\-Teshima|PiXEL|PopHD|POTO|PRESTiGE|QTZ|QUALiTY|R3MIX|RiFiFi|RiPiT|SANTACRUZ|Seimeisen|Slay3R|SUPERFLU|TARDiS|TAT|Team\.Arcedo|Themouche|TkHD|TLC|TMB|TSR|UTT|WaCkS|Winks|Xantar|XSPITFIRE911|Yangire\-Raws|Yarashii|ZTM)$'),
-('Regex_FR_tier_3', '(?i)[-.](Anime\.Heart|Aoi\.Project|Arcedo|BLV|BraD|BY_ORDER|D3T3R10R1TY|dRuIdE|Erai\-raws|Galactic|Alexis|HANAMi|Kaerizaki\.Fansub|kazuizui|KHAYA|KHFR|KushEnthusiast|LAZARUS|matheousse|MAX|Monkey\-D\.Lulu|NekoYu|NeoSG|Onii\-ChanSub|Pikari\.Teshima|QC63|RONiN|Scaph|TheFantastics|ToonsHub|Tsundere\.Raws|TTN|VARYG|WQM|Yangire\.Raws)$'),
-('Regex_FR_tier_4', '(?i)[-.](4FR|AiR3D|AiRDOCS|AiRFORCE|AiRLiNE|AiRTV|AKLHD|AMB3R|ANMWR|AVON|AYMO|AZR|BANKAi|BAWLS|BiPOLAR|BLACKPANTERS|BODIE|BOOLZ|BRiNK|BTT|CARAPiLS|CiELOS|CiNEMA|CMBHD|CoRa|COUAC|CRYPT0|D4KiD|DEAL|DiEBEX|DUPLI|DUSS|ENJOi|EUBDS|FHD|FiDELiO|FiDO|ForceBleue|FREAMON|FRENCHDEADPOOL2|FRiES|FUTiL|FWDHD|GHOULS|GiMBAP|GLiMMER|Goatlove|HERC|HiggsBoson|HiRoSHiMa|HYBRiS|HyDe|JMT|JoKeR|JUSTICELEAGUE|KAZETV|L0SERNiGHT|LaoZi|LeON|LOFiDEL|LOST|LOWIMDB|LUCKY|LYPSG|MAGiCAL|MANGACiTY|MAXAGAZ|MaxiBeNoul|McNULTY|MELBA|MiND|MORELAND|MUNSTER|MUxHD|N0Z00M|NERDHD|NERO|NrZ|NTK|OBSTACLE|OohLaLa|OOKAMI|PANZeR|PATHECROUTE|Penrose|PHoQUE|PiNKPANTERS|PKPTRS|PRiDEHD|PROPJOE|PURE|PUREWASTEOFBW|ROUGH|RUDE|Ryotox|S4LVE|SAFETY|SASHiMi|SEiGHT|SESKAPiLE|SharpHD|SHEEEiT|SHiNiGAMi|SiGeRiS|SILVIODANTE|SLEEPINGFOREST|SODAPOP|SPINE|SPOiLER|STRINGERBELL|Sunday26th|SUNRiSE|tFR|THENiGHTMAREiNHD|THiNK|THREESOME|TiMELiNE|TSuNaMi|UKDHD|UKDTV|ULSHD|Ulysse|UNSKiLLED|URY|USURY|VENUE|VFC|VoMiT|Wednesday29th|ZEST|ZiRCON)$'),
-('Regex_FR_tier_5', '(?i)[-.](ACOOL|AlioZ|ANONA|ARKRiL|ASPHiXiAS|AT|AViTECH|AZAZE|Balibalo|Bandix|bigZT|BLABLASTREAM|Boheme|BOL|BossBaby|Champion9|CINeHD|Copycomic|Cortex91|Cpasbien|CPB|CR4ZYTiME|CZ|DDLFRENCHORG|DOLL4R|Dread\.Team|Dropse|EASPORTS|EliteT|EXTREME|EZTV\.re|FERVEX|FGT|Firetown|FReeZeR|FUN|FUNKKY|FZTeam|GAIA|GHOSTSPiRiT|GHZ|GLaDOS|GOBO2S|GZR|HD2|HDMIDIMADRIDI|HEVCBay|HMiDiMADRiDi|Hush|JetAnime|JiHeff|KILLERMIX|KR4K3N|L\-O\-L|LiBERTAD|LION|LMPS|LNA3d|LTM|MACK4|Matmatha|MeMyl|METALLIKA|MGD|MKVXTEAM|Monchat|MONiCO|Moorea81|Moviz|Muxman|Mystic|MZC|MZiSYS|N3TFL1X|NEWCINE|NewZT|NG|NLX5|NoelMaison|NOMAD|NORRIS|nutella|OMERTA|Papaya|PiCKLES|PIKACHU|PREUMS|PULSE|Q7|qctimb3rlandqc|ReBoT|RELiC|ROLLED|RPZ|SANCTUAIRE|SCREEN|SHARKS|SHiFT|ShowFR|SKRiN|SP3CTR|Spow|STR4NGE|STVFRV|SubZero|T9|TeamSuW|TicaDow|Time2Watch|TIREXO|Tokushi|Tonyk|Torrent9|TORRiD|TOXIC|TSN999|TUTUTE|TVPSLO|UNiKORN|Upmix|VATFER|VERCLAM|ViKi47|Wakanim|WaNeZt|Wawa|WebAnime|WINCHESTER|WITA|YIFY|YTS|Zombie|ZONE|ZT|ZW)$'),
-('Regex_INTL_tier_1', '(?i)[-.](coffee|DON|IMNEWHERE|LM|RARBG|REBORN|SA89|SoLaR|TeamSyndicate|ZoroSenpai)$'),
-('Regex_INTL_tier_2', '(?i)[-.](c0kE|CtrlHD|D\-Z0N3|EbP|Geek|HiFi|LoRD|maniac|TayTo|VietHD|ZQ)$'),
-('Regex_INTL_tier_3', '(?i)[-.](BV|CRiSC|decibeL|FoRM|HiDt|HiP|iFT|MADSKY|SbR|WMING)$'),
-('Regex_INTL_tier_4', '(?i)[-.](ATELiER|BLOOM|BMF|eXterminator|faBR|HDMaNiAcS|IDE|LESTiN|LolHD|NCmt|NTb|RandomBytes|Skazhutin|Softboat)$'),
-('Regex_INTL_tier_5', '(?i)[-.](0BSiDiAN|AJP69|BakedFEL|BAT1|BSTD|Chotab|CJ|CRX|Dariush|E\.N\.D|E1|EA|EDPH|ELiTE|ENDSkY|ESiR|EXCiSION|FraMeSToR|GALAXY|GS88|GZ|hdalx|HQMUX|HR|iLoveHD|KASHMiR|Kitsune|LAZY|LiNG|luvBB|Natuyuki|NiBuRu|nmd|NyHD|ORiGEN|pcroland|Penumbra|playHD|Positive|PTer|RiCO|rightSIZE|RO|Rose3Thorn|rttr|SaNcTi|SiMPLE|SOP|SPHD|TBB|TDD|TnP|ViSUM|VLAD|W4NK3R|WiLF|xander|ZIMBO)$'),
-('Regex_INTL_tier_6', '(?i)[-.](ASD87|BRUTE|BTN|CART|CHD|EuReKA|GALVANiZE|HaB|HANDJOB|HDC|iON|Ivandro|j3rico|KnG|LEGi0N|Lulz|MaG|MTeam|NiP|ORBiT|P0W4HD|PTP|PuTao|ROCiNANTE|Slappy|ThD|WiKi|WiLDCAT)$');
+INSERT INTO regular_expressions (name, pattern, description) VALUES
+('Regex_INTL_tier_6', '(?i)[-.](ASD87|BRUTE|BTN|CART|CHD|EuReKA|GALVANiZE|HaB|HANDJOB|HDC|iON|Ivandro|j3rico|KnG|LEGi0N|Lulz|MaG|MTeam|NiP|ORBiT|P0W4HD|PTP|PuTao|ROCiNANTE|Slappy|ThD|WiKi|WiLDCAT)$', '');
 
 -- Custom Formats
 INSERT INTO custom_formats (name, description, include_in_rename) VALUES
@@ -103,18 +91,18 @@ INSERT INTO quality_group_members (quality_profile_name, quality_group_name, qua
 
 -- Profile Qualities (Priorities)
 INSERT INTO quality_profile_qualities (quality_profile_name, quality_name, quality_group_name, position, upgrade_until) VALUES
-('1080p', NULL, 'Group-Bluray-Webdl-1080p', 1, 1),
-('1080p', NULL, 'Group-HDTV-Webrip-1080p', 2, 0),
-('1080p', 'Remux-1080p', NULL, 3, 0),
-('1080p', NULL, 'Group-Bluray-Webdl-2160p', 4, 0),
-('1080p', NULL, 'Group-HDTV-Webrip-2160p', 5, 0),
-('1080p', 'Remux-2160p', NULL, 6, 0),
-('4K', NULL, 'Group-Bluray-Webdl-2160p', 1, 1),
-('4K', NULL, 'Group-HDTV-Webrip-2160p', 2, 0),
-('4K', 'Remux-2160p', NULL, 3, 0),
-('4K', NULL, 'Group-Bluray-Webdl-1080p', 4, 0),
-('4K', NULL, 'Group-HDTV-Webrip-1080p', 5, 0),
-('4K', 'Remux-1080p', NULL, 6, 0);
+('1080p', NULL, 'Group-Bluray-Webdl-1080p', 0, 1),
+('1080p', NULL, 'Group-HDTV-Webrip-1080p', 1, 0),
+('1080p', 'Remux-1080p', NULL, 2, 0),
+('1080p', NULL, 'Group-Bluray-Webdl-2160p', 3, 0),
+('1080p', NULL, 'Group-HDTV-Webrip-2160p', 4, 0),
+('1080p', 'Remux-2160p', NULL, 5, 0),
+('4K', NULL, 'Group-Bluray-Webdl-2160p', 0, 1),
+('4K', NULL, 'Group-HDTV-Webrip-2160p', 1, 0),
+('4K', 'Remux-2160p', NULL, 2, 0),
+('4K', NULL, 'Group-Bluray-Webdl-1080p', 3, 0),
+('4K', NULL, 'Group-HDTV-Webrip-1080p', 4, 0),
+('4K', 'Remux-1080p', NULL, 5, 0);
 
 -- Profile Custom Formats Scoring
 INSERT INTO quality_profile_custom_formats (quality_profile_name, custom_format_name, arr_type, score) VALUES
